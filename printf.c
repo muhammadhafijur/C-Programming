a963bc05edae8a0c50531f24141ccd860453d81c
MuhammadHafijur <hafijur.salah@gmail.com>
2022-08-18 12:51:24 +0600

    variable, comments

---
+++ printf.c
@@ -0,0 +1,21 @@
+// My first Program, It prints hello world
+
+/*
+
+    August 18, 2022
+
+*/
+
+#include<stdio.h>
+
+int main(){
+
+    int marks = 100;
+    int price = 50; // in Taka
+
+    float temperature = 30.5;
+
+    printf("Temp = %f", temperature);
+
+    return 0;
+}
---
+++ variable_declaration.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+
+    int price = 20 + 30;
+    int marks = 90;
+    printf("%d\n", price);
+
+    float temperature;
+
+    price = marks - 10;
+
+    printf("%d\n", price);
+
+    int price_of_sugar;
+
+    int price_of_potato;
+
+    int priceOfRice;
+    int priceOfJuice;
+}
