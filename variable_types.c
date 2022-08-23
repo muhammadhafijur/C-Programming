6c0d77d928703f77ef686d308ebd8ef3d07cc4c4
MuhammadHafijur <hafijur.salah@gmail.com>
2022-08-23 16:38:05 +0600

    data types in c

---
+++ variable_types.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+
+    int price = 10;
+    printf("price = %d\n", price);
+
+
+    float temparature = 30.5;
+    printf("temp = %f\n", temparature);
+
+    double temparature2 = 29.5;
+    printf("temp2 = %lf\n", temparature2);
+
+    char character = 'C';
+    printf("character = %c\n", character);
+
+
+    return 0;
+
+}
