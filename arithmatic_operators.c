601b58f8a8986316895cc6f3146866b2c8292b65
MuhammadHafijur <hafijur.salah@gmail.com>
2022-09-04 20:22:44 +0600

    arithmatic_operator

---
+++ arithmatic_operators.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int main() {
+
+//    int num1, num2;
+//    scanf("%d %d", &num1, &num2);
+
+//    int jogfol = num1 + num2;
+//    printf("%d + %d = %d\n", num1, num2, jogfol);
+//
+//    int biyogfol = num1 - num2;
+//    printf("%d - %d = %d\n", num1, num2, biyogfol);
+//
+//    int gunfol = num1 * num2;
+//    printf("%d * %d = %d\n", num1, num2, gunfol);
+//
+//    int vagfol = num1 / num2;
+//    printf("%d / %d = %d\n", num1, num2, vagfol);
+//
+//    int vagses = num1 % num2;
+//    printf("%d %% %d = %d\n", num1, num2, vagses);
+
+    double num1, num2;
+    scanf("%lf %lf", &num1, &num2);
+
+    double jogfol = num1 + num2;
+    printf("%lf + %lf = %lf\n", num1, num2, jogfol);
+
+    double biyogfol = num1 - num2;
+    printf("%lf - %lf = %lf\n", num1, num2, biyogfol);
+
+    double gunfol = num1 * num2;
+    printf("%lf * %lf = %lf\n", num1, num2, gunfol);
+
+    double vagfol = num1 / num2;
+    printf("%lf / %lf = %lf\n", num1, num2, vagfol);
+
+
+
+    return 0;
+}
